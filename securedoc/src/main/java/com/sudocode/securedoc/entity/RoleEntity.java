050480baf86f46ae5b98e3421564d4c299789851
sthembizo.molefi <sthembizo@psybergate.co.za>
2024-11-02 01:38:01 +0200

    Role entity class

---
+++ securedoc/src/main/java/com/sudocode/securedoc/entity/RoleEntity.java
@@ -0,0 +1,10 @@
+package com.sudocode.securedoc.entity;
+
+
+public class RoleEntity extends Auditable {
+
+    private String name;
+
+    private String authorities; //TODO : make this an enum
+
+}
