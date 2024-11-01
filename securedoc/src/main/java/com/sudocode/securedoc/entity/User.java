323520ebb9e70d44d6390fa24e92dea9b078a386
sthembizo.molefi <sthembizo@psybergate.co.za>
2024-11-02 01:34:47 +0200

    jpa annotations

--- securedoc/src/main/java/com/sudocode/securedoc/entity/User.java
+++ securedoc/src/main/java/com/sudocode/securedoc/entity/User.java
@@ -1,13 +1,31 @@
 package com.sudocode.securedoc.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
 import java.time.LocalDateTime;
 
-public class User extends Auditable{
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+@JsonInclude(JsonInclude.Include.NON_DEFAULT) // leaves out default values on serialization
+public class User extends Auditable {
 
+    @Column(updatable = false, unique = true, nullable = false)
     private String userId;
 
     private String firstName;
     private String lastName;
+    @Column(unique = true, nullable = false)
     private String email;
     private Integer loginAttempts;
     private LocalDateTime lastLogin;
@@ -18,7 +36,10 @@ public class User extends Auditable{
     private boolean accountNonLocked;
     private boolean enabled;
     private boolean mfa;
+    @JsonIgnore
     private String qrCodeSecret;
+
+    @Column(columnDefinition = "TEXT")
     private String qrCodeImgUri;
 
     private String roles; //TODO create roles class and map here with JPA
