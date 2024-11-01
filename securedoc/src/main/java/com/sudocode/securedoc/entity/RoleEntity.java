cf5b74e3598df144a5a5203125763e35895b0916
sthembizo.molefi <sthembizo@psybergate.co.za>
2024-11-02 01:38:39 +0200

    jpa annotations

--- securedoc/src/main/java/com/sudocode/securedoc/entity/RoleEntity.java
+++ securedoc/src/main/java/com/sudocode/securedoc/entity/RoleEntity.java
@@ -1,6 +1,19 @@
 package com.sudocode.securedoc.entity;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
 
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
 public class RoleEntity extends Auditable {
 
     private String name;
