b5d7c6efe7e0748029964052c31b9cf66b9405fd
sthembizo.molefi <sthembizo@psybergate.co.za>
2024-11-01 23:35:14 +0200

    Auditable class keeps track of the meta data on my domain objects

---
+++ securedoc/src/main/java/com/sudocode/securedoc/entity/Auditable.java
@@ -0,0 +1,49 @@
+package com.sudocode.securedoc.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.util.AlternativeJdkIdGenerator;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdAt","updatedAt"}, allowGetters = true)
+public abstract class Auditable {
+
+    @Id
+    @SequenceGenerator(name = "primary_key_seq", sequenceName = "primary_key_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "primary_key_seq")
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    /***
+     *  used to identify a specific resource in the database. like uuid
+     */
+    private String referenceId = new AlternativeJdkIdGenerator().generateId().toString();
+
+    @NotNull
+    private Long createdBy;
+
+    @NotNull
+    private Long updatedBy;
+
+    @NotNull
+    @CreatedDate
+    @Column(name = "created_at", nullable = false , updatable = false)
+    private LocalDateTime createdAt;
+
+    @CreatedDate
+    @Column(name = "updated_at", nullable = false )
+    private LocalDateTime updatedAt;
+}
+
+
