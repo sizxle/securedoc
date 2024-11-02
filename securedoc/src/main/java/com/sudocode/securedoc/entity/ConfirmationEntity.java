c4386287b21c723f9c3b3aceb8f1613f1db3b179
sthembizo.molefi <sthembizo@psybergate.co.za>
2024-11-02 15:10:30 +0200

    confirmation keys

---
+++ securedoc/src/main/java/com/sudocode/securedoc/entity/ConfirmationEntity.java
@@ -0,0 +1,38 @@
+package com.sudocode.securedoc.entity;
+
+
+import com.fasterxml.jackson.annotation.*;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "confirmations")
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
+public class ConfirmationEntity extends Auditable {
+
+
+    private String token;
+
+    @OneToOne(targetEntity = UserEntity.class, fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+    @JsonIdentityReference(alwaysAsId = true)
+    @JsonProperty("user_id")
+    private UserEntity user;
+
+    public ConfirmationEntity(UserEntity user) {
+        this.token = UUID.randomUUID().toString();
+        this.user = user;
+    }
+}
