8f22a9508ff6039fd9b2bc9728785aaefdeb27e4
sthembizo.molefi <sthembizo@psybergate.co.za>
2024-11-02 14:13:30 +0200

    AUTHORRITIES CONSTANTS

---
+++ securedoc/src/main/java/com/sudocode/securedoc/constant/Constants.java
@@ -0,0 +1,12 @@
+package com.sudocode.securedoc.constant;
+
+public class Constants {
+
+    public static final String ROLE_PREFIX = "ROLE_";
+    public static final String AUTHORITY_DELIMITER = ",";
+    public static final String USER_AUTHORITIES = "document:create,document:read,document:update,document:delete";
+    public static final String ADMIN_AUTHORITIES = "user:create,user:read,user:update,user:delete,document:create,document:read,document:update,document:delete";
+    public static final String SUPER_ADMIN_AUTHORITIES = "document:create,document:read,document:update,document:delete";
+    public static final String MANGER_AUTHORITIES = "document:create,document:read,document:update,document:delete";
+
+}
